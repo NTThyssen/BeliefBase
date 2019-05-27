809393730e1116c7d5aef5bf9149b7d75d0be336
bijannegari <bijannegari@hotmail.com>
2019-05-27 11:00:03 +0200

    Added printTruthTable function to main menu

--- src/TextualInterface.java
+++ src/TextualInterface.java
@@ -10,7 +10,8 @@ public class TextualInterface {
 
         System.out.println("* 1. View Knowledge Base *");
         System.out.println("* 2. Insert sentence     *");
-        System.out.println("* 3. Exit dis program yo *");
+        System.out.println("* 3. View Truth Table    *");
+        System.out.println("* 4. Exit dis program yo *");
         System.out.println("**************************");
     }
 
